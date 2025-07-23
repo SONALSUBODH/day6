a29490e8efdcca4df78e3b5ae1d6e0be74ef562c
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-23 19:19:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,21 +25,6 @@ resource "aws_security_group" "tf_sg" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
-resource "aws_instance" "web" {
-  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
-
-
 resource "aws_instance" "demo" {
   ami           = "var.ami_id"  
   instance_type = "var.instance_type"
